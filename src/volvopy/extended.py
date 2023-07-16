3aee2a68e98d4fc57da2853ce9d14e0fd0855fef
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-07-16 14:10:42 +0200

    INITIAL COMMIT

---
+++ src/volvopy/extended.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import __constants__ as vc
+import os
+import syslog
+
+import mausy5043_common.funfile as mf
+DEBUG = False
+HERE = os.path.realpath(__file__).split("/")
+# runlist id :
+MYID = HERE[-1]
+
+
+class Extended_Vehicle():
+    """Class to connect and interact with the Volvo Extended Vehicle API.
+
+    ref.:https://developer.volvocars.com/apis/extended-vehicle/v1/specification/
+    """
+    def __init__(self):
+        self.api_primary_key = vc.API_KEY[0]
+        self.api_secondary_key = vc.API_KEY[1]
+        self.api_token = vc.API_TOKEN
+        self.vin = vc.API_VIN
+        vin = self.vin  # noqa
+        resource = "NOT-AVAILABLE"  # noqa
+        api = 'extended'
+        self.base_url = f"{vc.API_SPECIFICATIONS[api]['servers'][0]['url']}"
+        self.call_urls = []
+        for path in vc.API_SPECIFICATIONS[api]['paths']:
+            url_path = eval(f"f'{path}'")
+            self.call_urls.append(f"{self.base_url}{url_path}")
+        print(f"Number of URLs: {len(self.call_urls)}")
+        for item in self.call_urls:
+            mf.syslog_trace(item, False, DEBUG)
+
+
+if __name__ == "__main__":
+    DEBUG = True
+    a = Extended_Vehicle()
---
+++ src/volvopy/location.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import __constants__ as vc
+import os
+import syslog
+
+import mausy5043_common.funfile as mf
+DEBUG = False
+HERE = os.path.realpath(__file__).split("/")
+# runlist id :
+MYID = HERE[-1]
+
+
+class Location():
+    """Class to connect and interact with the Volvo Location API.
+
+    ref.: https://developer.volvocars.com/apis/location/v1/specification/
+    """
+    def __init__(self):
+        self.api_primary_key = vc.API_KEY[0]
+        self.api_secondary_key = vc.API_KEY[1]
+        self.api_token = vc.API_TOKEN
+        self.vin = vc.API_VIN
+        vin = self.vin  # noqa
+        api = 'location'
+        self.base_url = f"{vc.API_SPECIFICATIONS[api]['servers'][0]['url']}"
+        self.call_urls = []
+        for path in vc.API_SPECIFICATIONS[api]['paths']:
+            url_path = eval(f"f'{path}'")
+            self.call_urls.append(f"{self.base_url}{url_path}")
+        print(f"Number of URLs: {len(self.call_urls)}")
+        for item in self.call_urls:
+            mf.syslog_trace(item, False, DEBUG)
+
+
+if __name__ == "__main__":
+    DEBUG = True
+    a = Location()
