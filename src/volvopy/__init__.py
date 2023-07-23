afb507dfb3468ce729eec7e4bf7a8e63df89f2e9
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-07-23 10:30:51 +0200

    update

--- src/volvopy/__init__.py
+++ src/volvopy/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-from .__constants__ import *
-from .connected import *
-from .energy import *
-from .extended import *
-from .location import *
+from . import __constants__
+from . import connected
+from . import energy
+from . import extended
+from . import location
+
+__all__ = [
+    "__constants__",
+    "connected",
+    "energy",
+    "extended",
+    "location",
+]
