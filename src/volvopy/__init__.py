620dca40b50d472c019a007387c9769e796e876a
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-07-23 13:14:43 +0200

    try empty file

--- src/volvopy/__init__.py
+++ src/volvopy/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-from . import __constants__
-from . import connected
-from . import energy
-from . import extended
-from . import location
-
-__all__ = [
-    "__constants__",
-    "connected",
-    "energy",
-    "extended",
-    "location",
-]
+# from . import __constants__
+# from . import connected
+# from . import energy
+# from . import extended
+# from . import location
+#
+# __all__ = [
+#     "__constants__",
+#     "connected",
+#     "energy",
+#     "extended",
+#     "location",
+# ]
